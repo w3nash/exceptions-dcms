cc181e82d86db6f37d65babe92322c6ed38630a9
Jonash Marcelino <thewwwnash@gmail.com>
2024-10-29 05:13:11 +0800

    Add routers and endpoints

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,26 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { patientRouter } from "./routers/patient";
+import { appointmentRouter } from "./routers/appointment";
+import { treatmentRouter } from "./routers/treatment";
+import { procedureRouter } from "./routers/procedure";
+import { userRouter } from "./routers/user";
+import { medicalHistoryRouter } from "./routers/medical_history";
+import { dentistRouter } from "./routers/dentist";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  patientRouter,
+  appointmentRouter,
+  treatmentRouter,
+  procedureRouter,
+  medicalHistoryRouter,
+  dentistRouter,
+  userRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
